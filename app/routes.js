33bf59a43fe6cc1fa0c4def37c59987b31a0fb97
ls-nomensa <93996837+ls-nomensa@users.noreply.github.com>
2023-03-06 17:30:31 +0000

    Create current CoP journey prototoype

--- app/routes.js
+++ app/routes.js
@@ -14,7 +14,7 @@ const router = govukPrototypeKit.requests.setupRouter()
 router.post('/training-provider/approvals/add/action/entry-method', function (req, res) {
   var addApprenticeDetails = req.session.data['add-apprentice-details']
 
-  if (addApprenticeDetails == "csv-file"){
+  if (addApprenticeDetails == "csv-file") {
     res.redirect('/training-provider/approvals/add/file-upload')
   }
   else if (addApprenticeDetails == "manually") {
@@ -25,7 +25,7 @@ router.post('/training-provider/approvals/add/action/entry-method', function (re
 router.post('/training-provider/approvals/add/action/select-journey', function (req, res) {
   var addApprenticeDetails2 = req.session.data['add-apprentice-details-2']
 
-  if (addApprenticeDetails2 == "existing-cohort"){
+  if (addApprenticeDetails2 == "existing-cohort") {
     res.redirect('/training-provider/approvals/choose-cohort')
   }
   else if (addApprenticeDetails2 == "new-cohort") {
@@ -36,7 +36,7 @@ router.post('/training-provider/approvals/add/action/select-journey', function (
 router.post('/training-provider/approvals/add/action/confirm-employer', function (req, res) {
   var confirmEmployer = req.session.data['confirm-employer']
 
-  if (confirmEmployer == "yes"){
+  if (confirmEmployer == "yes") {
     res.redirect('/training-provider/approvals/add/select-payments-pilot')
   }
   else {
@@ -49,18 +49,18 @@ router.post('/training-provider/approvals/action/recognise-prior-learning', func
   var pilotProgram = req.session.data['pilot-program']
   var deliveryMethod = req.session.data['delivery-method']
 
-  if (priorLearning == "yes"){
+  if (priorLearning == "yes") {
     res.redirect('/training-provider/approvals/recognise-prior-learning-details')
   }
-  else if (priorLearning == "no"){
-    if (pilotProgram == "No"){
+  else if (priorLearning == "no") {
+    if (pilotProgram == "No") {
       res.redirect('/training-provider/approvals/approve-details-non-pilot')
     }
     else {
-      if (deliveryMethod == "portable-flexi-job"){
+      if (deliveryMethod == "portable-flexi-job") {
         res.redirect('/training-provider/approvals/approve-details-pilot-pjf')
       }
-      else{
+      else {
         res.redirect('/training-provider/approvals/approve-details-pilot-non-pjf')
       }
     }
@@ -70,14 +70,14 @@ router.post('/training-provider/approvals/action/recognise-prior-learning-detail
   var pilotProgram = req.session.data['pilot-program']
   var deliveryMethod = req.session.data['delivery-method']
 
-  if (pilotProgram == "No"){
+  if (pilotProgram == "No") {
     res.redirect('/training-provider/approvals/approve-details-non-pilot')
   }
   else {
-    if (deliveryMethod == "portable-flexi-job"){
+    if (deliveryMethod == "portable-flexi-job") {
       res.redirect('/training-provider/approvals/approve-details-pilot-pjf')
     }
-    else{
+    else {
       res.redirect('/training-provider/approvals/approve-details-pilot-non-pjf')
     }
   }
@@ -87,14 +87,14 @@ router.post('/training-provider/approvals/add/action/personal-details', function
   var pilotProgram = req.session.data['pilot-program']
   var deliveryMethod = req.session.data['delivery-method']
 
-  if (pilotProgram == "No"){
+  if (pilotProgram == "No") {
     res.redirect('/training-provider/approvals/add/training-details-non-pilot')
   }
   else {
-    if (deliveryMethod == "portable-flexi-job"){
+    if (deliveryMethod == "portable-flexi-job") {
       res.redirect('/training-provider/approvals/add/training-details-pilot-pfj')
     }
-    else{
+    else {
       res.redirect('/training-provider/approvals/add/training-details-pilot-non-pfj')
     }
   }
@@ -104,7 +104,7 @@ router.post('/training-provider/approvals/add/action/personal-details', function
 router.post('/employer/apprentice/add/action/confirm-provider-details', function (req, res) {
   var useProvider = req.session.data['use-provider']
 
-  if (useProvider == "No"){
+  if (useProvider == "No") {
     res.redirect('/employer/apprentice/add/provider-details')
   }
   else {
@@ -115,7 +115,7 @@ router.post('/employer/apprentice/add/action/confirm-provider-details', function
 router.post('/employer/apprentice/add/action/start-adding-apprentices', function (req, res) {
   var whoIsAddingApprentices = req.session.data['WhoIsAddingApprentices']
 
-  if (whoIsAddingApprentices == "Provider"){
+  if (whoIsAddingApprentices == "Provider") {
     res.redirect('/employer/apprentice/add/message-for-training-provider')
   }
   else {
@@ -126,10 +126,21 @@ router.post('/employer/apprentice/add/action/start-adding-apprentices', function
 router.post('/employer/apprentice/add/action/approve-details', function (req, res) {
   var selection = req.session.data['selection']
 
-  if (selection == "Send"){
+  if (selection == "Send") {
     res.redirect('/employer/apprentice/add/notify-training-provider')
   }
   else {
     res.redirect('/employer/apprentice/add/approved')
+  }
+})
+
+router.post('/feature/COP-current/action/details-of-mismatch', function (req, res) {
+  var SubmitStatusViewModel = req.session.data['SubmitStatusViewModel']
+
+  if (SubmitStatusViewModel == "Confirm") {
+    res.redirect('/feature/COP-current/change-apprentice-details')
+  }
+  else {
+    res.redirect('/feature/COP-current/apprentice-details')
   }
 })
