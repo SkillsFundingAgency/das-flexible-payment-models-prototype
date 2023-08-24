3a65c7011909a02e26394eb1261accaee3f77c2b
staylor-design <staylor@nomensa.com>
2023-08-24 17:15:06 +0100

    cop
    adding dynamic java script to show price on, added effective from date back in.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,4 +5,12 @@
 
 window.GOVUKPrototypeKit.documentReady(() => {
   // Add JavaScript here
+let total=8500;
+  $('#apprenticeship-training-price, #apprenticeship-epa-price').focusout(function () {
+    total = Number($('#apprenticeship-training-price').val().replaceAll(',', '')) + Number($('#apprenticeship-epa-price').val().replaceAll(',', ''));
+    
+    $('#apprenticeship-total-price').text('£' + total.toLocaleString('en-GB'));
+  });
+
+  $('#apprenticeship-total-price').text('£' + total.toLocaleString('en-GB'));
 })
