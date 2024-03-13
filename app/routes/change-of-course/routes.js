269c6781d6f5ab9cb0ce40d8bd6f740585bf9d3a
ls-nomensa <93996837+ls-nomensa@users.noreply.github.com>
2024-03-13 18:03:07 +0000

    Updates to change of course designs

--- app/routes/change-of-course/routes.js
+++ app/routes/change-of-course/routes.js
@@ -3,14 +3,14 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 
 // Employer
-router.post('/change-of-course/04-price-change/action', function (req, res) {
+router.post('/change-of-course/training-provider/price-change/action', function (req, res) {
     var useProvider = req.session.data['change-price']
 
     if (useProvider == "no") {
-        res.redirect('/change-of-course/05-confirm-change-of-course')
+        res.redirect('/change-of-course/training-provider/confirm-change-of-course')
     }
     else {
-        res.redirect('/change-of-course/04-price-change-2')
+        res.redirect('/change-of-course//training-provider/update-price')
     }
 })
 
