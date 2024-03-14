10306e27ae3a78f022fcd4c37fdb2311b28e2415
ls-nomensa <93996837+ls-nomensa@users.noreply.github.com>
2024-03-14 15:31:45 +0000

    Update index pages and file structure

--- app/routes/change-of-course/routes.js
+++ app/routes/change-of-course/routes.js
@@ -3,14 +3,14 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 
 // Employer
-router.post('/change-of-course/training-provider/price-change/action', function (req, res) {
+router.post('/change-of-course/training-provider/v1/price-change/action', function (req, res) {
     var useProvider = req.session.data['change-price']
 
     if (useProvider == "no") {
-        res.redirect('/change-of-course/training-provider/confirm-change-of-course')
+        res.redirect('/change-of-course/training-provider/v1/confirm-change-of-course')
     }
     else {
-        res.redirect('/change-of-course/training-provider/update-price')
+        res.redirect('/change-of-course/training-provider/v1/update-price')
     }
 })
 
