a9feb104ad828e4dd3b3046ac8e0b348ceaa92c4
ls-nomensa <93996837+ls-nomensa@users.noreply.github.com>
2024-04-11 10:21:31 +0100

    Update course to standard

--- app/routes.js
+++ app/routes.js
@@ -8,7 +8,7 @@ require('./routes/feature.js');
 require('./routes/1/employer.js');
 require('./routes/1/training-provider.js');
 
-require('./routes/change-of-course/routes.js');
+require('./routes/change-of-standard/routes.js');
 require('./routes/change-of-status/routes.js');
 
 module.exports = router
