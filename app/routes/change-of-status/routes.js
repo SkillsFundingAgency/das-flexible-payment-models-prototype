0e9271f22a18804df1b4813f835bfc2bb5759d62
ls-nomensa <93996837+ls-nomensa@users.noreply.github.com>
2024-04-12 10:15:30 +0100

    Create v2 for change of status
    Content updates

--- app/routes/change-of-status/routes.js
+++ app/routes/change-of-status/routes.js
@@ -1,8 +1,28 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-
+// Version 1
 router.post('/change-of-status/training-provider/v1/edit-learner-status/action', function (req, res) {
+    var stoptp = req.session.data['stoptp']
+
+    if (stoptp == "yes") {
+        res.redirect('../pause-date')
+    }
+    else if 
+    (stoptp == "continuing") {
+        res.redirect('../resume-date')
+    }
+    else if 
+    (stoptp == "redundancy") {
+        res.redirect('../stop-date-redundant')
+    }
+    else {
+        res.redirect('../stop-date')
+    }
+})
+
+// Version 2
+router.post('/change-of-status/training-provider/v2/edit-learner-status/action', function (req, res) {
     var stoptp = req.session.data['stoptp']
 
     if (stoptp == "yes") {
