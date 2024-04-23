fd4345a44eb4397de5561485669b7076e7e1dafa
ls-nomensa <93996837+ls-nomensa@users.noreply.github.com>
2024-04-23 13:50:34 +0100

    Update the change of payment status

--- app/routes/change-of-status/routes.js
+++ app/routes/change-of-status/routes.js
@@ -40,3 +40,25 @@ router.post('/change-of-status/training-provider/v2/edit-learner-status/action',
         res.redirect('../stop-date')
     }
 })
+
+// Payment Status
+router.post('/change-of-status/employer/payment-status/v1/break-freeze/action', function (req, res) {
+    var freeze = req.session.data['freeze']
+
+    if (freeze == "no") {
+        res.redirect('../')
+    }
+    else {
+        res.redirect('../freeze-confirmed')
+    }
+})
+router.post('/change-of-status/employer/payment-status/v1/break-unfreeze/action', function (req, res) {
+    var unfreeze = req.session.data['unfreeze']
+
+    if (unfreeze == "no") {
+        res.redirect('../')
+    }
+    else {
+        res.redirect('../unfreeze-confirmed')
+    }
+})
