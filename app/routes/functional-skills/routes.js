f218c38424fa14de0200a5c3fa080478e7dfbbd4
ls-nomensa <93996837+ls-nomensa@users.noreply.github.com>
2024-06-19 18:20:24 +0100

    Create version 3 post approvals

--- app/routes/functional-skills/routes.js
+++ app/routes/functional-skills/routes.js
@@ -22,3 +22,27 @@ if
         res.redirect('../stop-date')
     }
 })
+
+// Version 3
+
+router.post('/functional-skills/post-approval/v3/withdraw/action', function (req, res) {
+    var continuewithdrawal = req.session.data['continue-withdrawal']
+
+if (continuewithdrawal == "no") {
+        res.redirect('../index')
+    }
+    else {
+        res.redirect('../reason--withdrawal')
+    }
+})
+
+router.post('/functional-skills/post-approval/v3/reason--withdrawal/action', function (req, res) {
+    var reasonforwithdrawal = req.session.data['reason-for-withdrawal']
+if 
+    (reasonforwithdrawal == "Apprenticeship never started") {
+        res.redirect('../confirm--withdrawal')
+    }
+    else {
+        res.redirect('../stop-date')
+    }
+})
