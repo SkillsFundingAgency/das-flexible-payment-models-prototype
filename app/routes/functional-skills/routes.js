64a0fcb14cc15db6977f8f1586dac751b7cee24d
ls-nomensa <93996837+ls-nomensa@users.noreply.github.com>
2024-07-15 12:13:23 +0100

    Add version 5 for post approval

--- app/routes/functional-skills/routes.js
+++ app/routes/functional-skills/routes.js
@@ -134,3 +134,49 @@ if (continuewithdrawal == "no") {
         res.redirect('../confirm--withdrawal-unfunded')
     }
 })
+
+// Version 5
+
+router.post('/functional-skills/post-approval/v5/withdraw/action', function (req, res) {
+    var continuewithdrawal = req.session.data['continue-withdrawal']
+
+if (continuewithdrawal == "no") {
+        res.redirect('../index')
+    }
+    else {
+        res.redirect('../reason--withdrawal')
+    }
+})
+
+router.post('/functional-skills/post-approval/v5/reason--withdrawal/action', function (req, res) {
+    var reasonforwithdrawal = req.session.data['reason-for-withdrawal']
+if 
+    (reasonforwithdrawal == "Apprenticeship never started") {
+        res.redirect('../confirm--withdrawal')
+    }
+    else {
+        res.redirect('../stop-date')
+    }
+})
+
+router.post('/functional-skills/post-approval/v5/stop-date/action', function (req, res) {
+    var year = req.session.data['withdrawal-year']
+
+if (year >= "2024") {
+        res.redirect('../funding-warning')
+    }
+    else {
+        res.redirect('../confirm--withdrawal')
+    }
+})
+
+router.post('/functional-skills/post-approval/v5/funding-warning/action', function (req, res) {
+    var continuewithdrawal = req.session.data['continue-withdrawal']
+
+if (continuewithdrawal == "no") {
+        res.redirect('../')
+    }
+    else {
+        res.redirect('../confirm--withdrawal-unfunded')
+    }
+})
