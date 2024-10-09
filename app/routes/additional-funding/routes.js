51710bc9578162ee17f8a076355717a6b7e2d1a1
ls-nomensa <93996837+ls-nomensa@users.noreply.github.com>
2024-10-09 17:28:16 +0100

    Additional funding post approval

--- app/routes/additional-funding/routes.js
+++ app/routes/additional-funding/routes.js
@@ -5,12 +5,17 @@ const router = govukPrototypeKit.requests.setupRouter()
 // V1
 router.post('/additional-funding/pre-approval/v1/ehc-plan/action', function (req, res) {
     var ehcplan = req.session.data['ehc-plan']
-
+    var journey = req.session.data['journey']
     if (ehcplan == "yes") {
         res.redirect('/additional-funding/pre-approval/v1/share-ehc-plan')
     }
     else {
-        res.redirect('../recognise-prior-learning')
+        if (journey == "post-add") {
+        res.redirect('/additional-funding/post-approval/v1/check-answers')
+        }
+        else {
+            res.redirect('../recognise-prior-learning')
+        }
     }
 })
 router.post('/additional-funding/pre-approval/v1/care-leaver/action', function (req, res) {
@@ -21,5 +26,25 @@ router.post('/additional-funding/pre-approval/v1/care-leaver/action', function (
     }
     else {
         res.redirect('../ehc-plan')
+    }
+})
+router.post('/additional-funding/pre-approval/v1/share-care-leaver/action', function (req, res) {
+    var journey = req.session.data['journey']
+
+    if (journey == "post-add") {
+        res.redirect('/additional-funding/post-approval/v1/check-answers')
+    }
+    else {
+        res.redirect('../recognise-prior-learning')
+    }
+})
+router.post('/additional-funding/pre-approval/v1/share-ehc-plan/action', function (req, res) {
+    var journey = req.session.data['journey']
+
+    if (journey == "post-add") {
+        res.redirect('/additional-funding/post-approval/v1/check-answers')
+    }
+    else {
+        res.redirect('../recognise-prior-learning')
     }
 })
